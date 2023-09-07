4e6f5c45c6a8d54e092454a8892178963a55f009
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 <anzymen@gmail.com>
2023-09-07 09:41:14 +0200

    try to edit my todo

--- index.js
+++ index.js
@@ -4,67 +4,58 @@ const listContainer = document.getElementById("list-container");
 const formGroup = document.getElementById("form-group");
 const animate = document.getElementsByClassName(".checked");
 const removed = document.getElementsByClassName(".removed");
-const back = document.querySelector('.btn');
+const back = document.querySelector(".btn");
 // back.classList.add('animate__animated', 'animate__bounceOutLeft');
 
-back.addEventListener('click', () => {
-    back.classList.add('animate__animated', 'animate__bounceOutLeft');
-    // do something
-});
+// back.addEventListener('click', () => {
+//     back.classList.add('animate__animated', 'animate__bounceOutLeft');
+// });
 
 function addTask() {
-    if (inputBox.value === '' || inputBox.value === null) {
-        alert("Please enter the task");
-        e.preventDefault();
-    }
-    else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    
-    inputBox.value = "";
-    saveData();
+  if (inputBox.value === "" || inputBox.value === null) {
+    alert("Please enter the task");
+    e.preventDefault();
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+
+  inputBox.value = "";
+  saveData();
 }
-listContainer.addEventListener("click", function (e) {
+listContainer.addEventListener(
+  "click",
+  function (e) {
     if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-        // e.target.classList.add("animate");
-        saveData();
+      e.target.classList.toggle("checked");
+      // e.target.classList.add("animate");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.classList.toggle("removed");
+      e.target.parentElement.remove();
+      saveData();
     }
-    else if (e.target.tagName === "SPAN") {
-        e.target.classList.toggle("removed");
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false);
+  },
+  false
+);
 
 function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML);
+  localStorage.setItem("data", listContainer.innerHTML);
 }
 function showTask() {
-    listContainer.innerHTML = localStorage.getItem("data");
+  listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
 
-function goBackWithDelay(delayMillis) {
-    setTimeout(function() {
-        goBack();
-    }, delayMillis);
-}
-
-function goBack() {
-    window.history.back();
-}
-
 // document.querySelector("button").addEventListener("click", (e) => {
-    // if (inputBox.value === '' || inputBox.value === null) {
-    //     alert("Please enter the task");
-    // }
-    // e.preventDefault();
+// if (inputBox.value === '' || inputBox.value === null) {
+//     alert("Please enter the task");
+// }
+// e.preventDefault();
 // });
 // animate.addEventListener('click', () => {
 //     animate.classList.add('animate__animated', 'animate__flipInY');
