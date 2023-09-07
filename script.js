f21aa1fcdd7e192c52c8f0edb8a23e0485f90488
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 <anzymen@gmail.com>
2023-09-07 12:00:08 +0200

    done adding my edit button

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+const itemsArray = localStorage.getItem("items")
+  ? JSON.parse(localStorage.getItem("items"))
+  : [];
+const inputText = document.querySelector(".input-text");
+const entering = document.querySelector(".entering");
 
-document.querySelector("#enter").addEventListener("click", () => {
-  const item = document.querySelector("#item")
-  createItem(item)
-})
+entering.addEventListener("click", function () {
+  // Check if the inputText element is null or its value is empty
+  if (!inputText || inputText.value.trim() === "") {
+    alert("Please enter the task!");
+  } else {
+    document.querySelector("#enter").addEventListener("click", () => {
+      const item = document.querySelector("#item");
+      createItem(item);
+    });
 
-document.querySelector("#item").addEventListener("keypress", (e) => {
-  if(e.key === "Enter"){
-    const item = document.querySelector("#item")
-    createItem(item)
+    document.querySelector("#item").addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        const item = document.querySelector("#item");
+        createItem(item);
+      }
+    });
   }
-})
+});
 
-function displayDate(){
-  let date = new Date()
-  date = date.toString().split(" ")
-  date = date[1] + " " + date[2] + " " + date[3] 
-  document.querySelector("#date").innerHTML = date 
+function displayDate() {
+  let date = new Date();
+  date = date.toString().split(" ");
+  date = date[1] + " " + date[2] + " " + date[3];
+  document.querySelector("#date").innerHTML = date;
 }
 
-function displayItems(){
-  let items = ""
-  for(let i = 0; i < itemsArray.length; i++){
+function displayItems() {
+  let items = "";
+  for (let i = 0; i < itemsArray.length; i++) {
     items += `<div class="item">
                 <div class="input-controller">
                   <textarea disabled>${itemsArray[i]}</textarea>
@@ -34,75 +45,78 @@ function displayItems(){
                   <button class="saveBtn">Save</button>
                   <button class="cancelBtn">Cancel</button>
                 </div>
-              </div>`
+              </div>`;
   }
-  document.querySelector(".to-do-list").innerHTML = items
-  activateDeleteListeners()
-  activateEditListeners()
-  activateSaveListeners()
-  activateCancelListeners()
+  document.querySelector(".to-do-list").innerHTML = items;
+  activateDeleteListeners();
+  activateEditListeners();
+  activateSaveListeners();
+  activateCancelListeners();
 }
 
-function activateDeleteListeners(){
-  let deleteBtn = document.querySelectorAll(".deleteBtn")
+function activateDeleteListeners() {
+  let deleteBtn = document.querySelectorAll(".deleteBtn");
   deleteBtn.forEach((dB, i) => {
-    dB.addEventListener("click", () => { deleteItem(i) })
-  })
+    dB.addEventListener("click", () => {
+      deleteItem(i);
+    });
+  });
 }
 
-function activateEditListeners(){
-  const editBtn = document.querySelectorAll(".editBtn")
-  const updateController = document.querySelectorAll(".update-controller")
-  const inputs = document.querySelectorAll(".input-controller textarea")
+function activateEditListeners() {
+  const editBtn = document.querySelectorAll(".editBtn");
+  const updateController = document.querySelectorAll(".update-controller");
+  const inputs = document.querySelectorAll(".input-controller textarea");
   editBtn.forEach((eB, i) => {
-    eB.addEventListener("click", () => { 
-      updateController[i].style.display = "block"
-      inputs[i].disabled = false })
-  })
+    eB.addEventListener("click", () => {
+      updateController[i].style.display = "block";
+      inputs[i].disabled = false;
+    });
+  });
 }
 
-function activateSaveListeners(){
-  const saveBtn = document.querySelectorAll(".saveBtn")
-  const inputs = document.querySelectorAll(".input-controller textarea")
+function activateSaveListeners() {
+  const saveBtn = document.querySelectorAll(".saveBtn");
+  const inputs = document.querySelectorAll(".input-controller textarea");
   saveBtn.forEach((sB, i) => {
     sB.addEventListener("click", () => {
-      updateItem(inputs[i].value, i)
-    })
-  })
+      updateItem(inputs[i].value, i);
+    });
+  });
 }
 
-function activateCancelListeners(){
-  const cancelBtn = document.querySelectorAll(".cancelBtn")
-  const updateController = document.querySelectorAll(".update-controller")
-  const inputs = document.querySelectorAll(".input-controller textarea")
+function activateCancelListeners() {
+  const cancelBtn = document.querySelectorAll(".cancelBtn");
+  const updateController = document.querySelectorAll(".update-controller");
+  const inputs = document.querySelectorAll(".input-controller textarea");
   cancelBtn.forEach((cB, i) => {
     cB.addEventListener("click", () => {
-      updateController[i].style.display = "none"
-      inputs[i].disabled = true
-      inputs[i].style.border = "none"
-    })
-  })
+      updateController[i].style.display = "none";
+      inputs[i].disabled = true;
+      inputs[i].style.border = "none";
+    });
+  });
 }
 
-function createItem(item){
-  itemsArray.push(item.value)
-  localStorage.setItem('items', JSON.stringify(itemsArray))
-  location.reload()
+function createItem(item) {
+  itemsArray.push(item.value);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  location.reload();
 }
 
-function deleteItem(i){
-  itemsArray.splice(i,1)
-  localStorage.setItem('items', JSON.stringify(itemsArray))
-  location.reload()
+function deleteItem(i) {
+  itemsArray.splice(i, 1);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  location.reload();
 }
 
-function updateItem(text, i){
-  itemsArray[i] = text
-  localStorage.setItem('items', JSON.stringify(itemsArray))
-  location.reload()
+function updateItem(text, i) {
+  itemsArray[i] = text;
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  location.reload();
 }
 
-window.onload = function() {
-  displayDate()
-  displayItems()
+window.onload = function () {
+  displayDate();
+  displayItems();
 };
